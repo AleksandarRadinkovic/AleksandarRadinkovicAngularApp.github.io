5e4253fdcb47af4cd6ae092d57ac3cad4ae56dea
radinkovica <acor995@gmail.com>
2022-02-22 14:19:12 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<div>Test</div>',
 })
 export class AppComponent {
-  title = 'taskApp';
+  pageTitle: string = 'Task App';
 }
