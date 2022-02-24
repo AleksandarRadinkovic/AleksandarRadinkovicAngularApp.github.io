cbe3c8c1a866ad54b965900d75ab177b41ef7f1d
radinkovica <acor995@gmail.com>
2022-02-24 15:29:51 +0100

    products in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<div>Test</div>',
+  template: `<div>
+    <h1>{{ pageTitle }}</h1>
+    <pm-products></pm-products>
+  </div> `,
 })
 export class AppComponent {
-  pageTitle: string = 'Task App';
+  pageTitle: string = 'Task Managment';
 }
